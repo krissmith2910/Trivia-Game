dded481773c56d160bc6537987387c0f56f4a7e9
Kristopher Smith <ksmith2910@outlook.com>
2019-08-12 14:53:01 -0600

    added code to all files

---
+++ assets/javascript/app.js
@@ -0,0 +1,113 @@
+$('#start').on('click', function () {
+    
+    game.start();
+})
+
+var questions = [{
+    question: "What are wolves afraid of?",
+    answers: [" Bears ", " Lightning ", " Humans ", " Guns ", " Fire "],
+    correctAnswer: " Humans "
+}, {
+    question: "How many species of wolves are there?",
+    answers: [" 10 ", " 3 ", " 8 ", " 26 ", " 1 "],
+    correctAnswer: " 3 "
+}, {
+    question: "How long do wolves live in the wild?",
+    answers: [" 6 to 8 years ", " 1 to 4 years ", " 6 to 11 years ", " 10 to 13 years ", " 15 to 17 years "],
+    correctAnswer: " 6 to 8 years "
+}, {
+    question: "What is a small wolf called?",
+    answers: [" pup ", " puppy ", " kid ", " young ", " cub "],
+    correctAnswer: " pup "
+}, {
+    question: "How fast can a wolf run?",
+    answers: [" 40mph ", " 20 to 25mph ", " 45mph ", " 31 to 37mph ", " 50mph "],
+    correctAnswer: " 31 to 37mph "
+}, 
+];
+
+var game = {
+    correct: 0,
+    incorrect: 0,
+    // seconds
+    counter: 30,
+    countdown: function () {
+        // created counter function that will 'start' below
+        game.counter--;
+        $('#counter').html(game.counter);
+        if (game.counter <= 0) {
+            console.log("Time is up");
+            // this method will run when game is done
+            game.done();
+        }
+    },
+    start: function () {
+        // insert time, 1000seconds
+        timer = setInterval(game.countdown,1000);
+        $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter">30</span> Seconds</h2>');
+        $('#start').remove();
+        for (var i = 0; i < questions.length; i++) {
+            // append to subwrapper
+            $('#subwrapper').append('<h2>' + questions[i].question + '</h2');
+            // subloop
+            for (var j = 0; j < questions[i].answers.length; j++) {
+                $("#subwrapper").append("<input type='radio' name='question-" +i+ "' value='" + questions[i].answers[j] + "'>" + questions[i].answers[j]);
+            }
+        }
+    },
+    done: function(){
+        $.each($("input[name='question-0']:checked"),function() {
+            if($(this).val()==questions[0].correctAnswer){
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($("input[name='question-1']:checked"),function() {
+            if($(this).val()==questions[1].correctAnswer){
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($("input[name='question-2']:checked"),function() {
+            if($(this).val()==questions[2].correctAnswer){
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($("input[name='question-3']:checked"),function() {
+            if($(this).val()==questions[3].correctAnswer){
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($("input[name='question-4']:checked"),function() {
+            if($(this).val()==questions[4].correctAnswer){
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($("input[name='question-5']:checked"),function() {
+            if($(this).val()==questions[5].correctAnswer){
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        this.result();
+    },
+    result: function(){
+        clearInterval(timer);
+        $('#subwrapper h2').remove();
+        $('#subwrapper').html("<h3>All done!</h3>");
+        // print all correct and incorrect answers
+        $('#subwrapper').append("<h3>Correct Answers: "+this.correct+"</h3>");
+        $('#subwrapper').append("<h3>Incorrect Answers: "+this.incorrect+"</h3>");
+        // print questions not answered
+        $('#subwrapper').append("<h3>Unanswered: "+(questions.length-(this.incorrect+this.correct))+"</h3>");
+    }
+}
